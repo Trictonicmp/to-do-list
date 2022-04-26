ac6b6f32ec3eb3c1cab379e51f4db2f5478c2bc2
Trictonicmp <code_shadow@hotmail.com>
2022-04-26 18:42:08 -0500

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    assetModuleFilename: 'images/[hash][ext][query]'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader", 
+          "css-loader"
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 9000,
+    compress: true
+  },
+};
