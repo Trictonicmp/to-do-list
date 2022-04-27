f7da4154cb43a181ee22ff4e333c9d361d3032e1
Trictonicmp <code_shadow@hotmail.com>
2022-04-27 12:57:44 -0500

    Add favicon icon

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,3 @@ import '../css/main.css';
 import DataHandler from './DataHandler.js';
 
 const dataHandler = new DataHandler();
-
-/* const listHandler = new ListHandler();
-const inputForm = document.forms[0];
-
-inputForm.onsubmit = (event) => {
-  event.preventDefault();
-  let itemDescription = inputForm[0].value;
-  listHandler.addItem(itemDescription);
-  inputForm.reset();
-  inputForm[0].focus();
-}
- */
